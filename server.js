82c3333a1a63910d3044cb67ab463c3e63854a59
ashkan <ashkanabdollahi19@gmail.com>
2024-03-01 18:53:21 +0330

    fixing unzipping bugs

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ express.static.mime.define({'application/wasm': ['wasm']});
 // Serve static files from the public directory
 app.use('/public', express.static('public'));
 
-app.get('unity/upload', (req, res) => {
+app.get('/unity/upload', (req, res) => {
     res.sendFile(__dirname + '/public/upload.html');
 });
 
-app.get('unity/games', (req, res) => {
+app.get('/unity/games', (req, res) => {
     res.sendFile(__dirname + '/public/manageGames.html');
 });
 
@@ -27,14 +27,14 @@ app.use((req, res, next) => {
 });
 
 // After the user routes:
-app.use('unity/api/games', gameRoutes);
+app.use('/unity/api/games', gameRoutes);
 
 // Then, after the asset routes:
-app.use('unity/api/users', userRoutes);
+app.use('/unity/api/users', userRoutes);
 
 // Middleware to serve static files from 'public' directory
 app.use(express.static('public'));
-app.use('unity/api/assets', assetRoutes);
+app.use('/unity/api/assets', assetRoutes);
 
 // Basic route for testing the server
 app.get('/unity', (req, res) => {
